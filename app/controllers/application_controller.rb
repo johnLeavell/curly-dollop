35981a86fab8126a133e4c56c0943863b89b2b23
John Leavell <johnleavell@gmail.com>
2023-04-26 19:02:59 -0500

    added user model

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,7 @@
 class ApplicationController < ActionController::Base
+
+  def homepage
+    render( template: "misc_templates/home")
+  end
+
 end
---
+++ app/models/user.rb
@@ -0,0 +1,9 @@
+class User < ApplicationRecord
+  validates :email, :uniqueness => { 
+    :case_sensitive => false }
+
+  validates :email, :presence => true
+
+  has_secure_password
+  
+end
